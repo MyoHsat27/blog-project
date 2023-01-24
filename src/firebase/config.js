a605eec1b160240704f2ec58b8fd6694c851bc37
Myo Hsat <hsatmyo@gmail.com>
2023-01-24 17:31:00 +0630

    first

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCamDU6ii2mnGGkaa4VXJ6XJIIY-y0Yj1Y",
+    authDomain: "blog-project-956ea.firebaseapp.com",
+    projectId: "blog-project-956ea",
+    storageBucket: "blog-project-956ea.appspot.com",
+    messagingSenderId: "702968370693",
+    appId: "1:702968370693:web:7239200b38d1079da8ee6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
